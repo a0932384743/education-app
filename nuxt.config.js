c95e4ed923686829af013cc1ea39f0c24a3b9648
MSI\user <a0932384743@gmail.com>
2024-06-18 10:53:19 +0800

    移除地圖套件

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
   css: ['~/assets/css/scrollbar.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/echart'],
+  plugins: ['~/plugins/echarts'],
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
--- types/global.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.json'
-
-declare module 'echarts-gl/charts'
-declare module 'echarts-gl/components'
-
-declare module 'vue-echarts' {
-  export * as ECharts from 'vue-echarts'
-}
