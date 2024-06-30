e574ae8d4000daeac55e78df6f84a966909aefbf
a0932 <a0932384743@gmail.com>
2024-06-30 23:38:55 +0800

    事件

--- pages/monitoring-item-management/alert-management/connection-unit.vue
+++ pages/monitoring-item-management/alert-management/connection-unit.vue
@@ -1,10 +1,109 @@
 <template>
-  <v-container class="fill-height" fluid> </v-container>
+  <v-row>
+    <v-col :cols="12">
+      <table-card title="告警管理-連線單位" :items="items">
+        <template
+          #default="{
+            search,
+            footerProps,
+            itemPerPage,
+            page,
+            items,
+            loading,
+            headerProps,
+          }"
+        >
+          <v-data-table
+            :headers="headers"
+            :items="items"
+            :search="search"
+            :page="page"
+            :items-per-page="itemPerPage"
+            :footer-props="footerProps"
+            :header-props="headerProps"
+            fixed-header
+            :loading="loading"
+            hide-default-footer
+          >
+            <template #[`item.connectUnit`]="{ item }">
+              <td
+                :class="item.isAlerting=== 'On'  ? 'success' : 'secondary'"
+                nowrap="nowrap"
+                style="border-color:white !important;"
+              >
+                {{ item.connectUnit || '-' }}
+              </td>
+            </template>
+            <template #[`item.isAlerting`]="{ item }">
+              <div v-if="!item.editable">{{ item.isAlerting }}</div>
+              <v-switch
+                v-else
+                v-model="item.isAlerting"
+                small
+                false-value="Off"
+                true-value="On"
+                color="info"
+                label=""
+              />
+            </template>
+            <template #[`item.operate`]="{ item }">
+              <v-btn
+                small
+                :disabled="Object.keys(item).some((key) => item[key] === '')"
+                :color="item.editable ? 'secondary' : 'info'"
+                @click="
+                  Object.keys(item).every((key) => item[key])
+                    ? (item.editable = !item.editable)
+                    : (item.editable = true)
+                "
+              >
+                <v-icon size="20">mdi-pencil</v-icon>
+                <span class="d-none d-sm-inline-block">{{ $t('edit') }}</span>
+              </v-btn>
+            </template>
+          </v-data-table>
+        </template>
+      </table-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
+import items from '~/assets/json/connection-unit.json';
+import { statusMap } from '~/utils/statusMap';
 export default {
   name: 'ConnectionUnit',
   layout: 'admin-layout',
+  data() {
+    return {
+      statusMap,
+    };
+  },
+  computed: {
+    headers() {
+      return [
+        {
+          text: this.$t('connect.unit.id'),
+          value: 'id',
+          width: 100
+        },
+        {
+          text: this.$t('connect.unit.name'),
+          value: 'connectUnit',
+        },
+        {
+          text: this.$t('interface.alerting'),
+          value: 'isAlerting',
+        },
+        {
+          text: this.$t('operate'),
+          value: 'operate',
+        },
+      ];
+    },
+    items() {
+      return items.map((item) => ({ ...item, editable: false }));
+    },
+  },
 };
 </script>
--- pages/monitoring-item-management/alert-management/equipment-category.vue
+++ pages/monitoring-item-management/alert-management/equipment-category.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row>
     <v-col :cols="12">
-      <table-card title="監控項目-告警管理-設備類" :items="items">
+      <table-card title="告警管理-設備類" :items="items">
         <template
           #default="{
             search,
@@ -10,7 +10,7 @@
             page,
             items,
             headerProps,
-            loading
+            loading,
           }"
         >
           <v-data-table
@@ -25,6 +25,15 @@
             fixed-header
             hide-default-footer
           >
+            <template #[`item.device`]="{ item }">
+              <td
+                :class="item.is_alert=== 'On'  ? 'success' : 'secondary'"
+                nowrap="nowrap"
+                style="border-color:white !important;"
+              >
+                {{ item.device || '-' }}
+              </td>
+            </template>
             <template #[`item.is_alert`]="{ item }">
               <div v-if="!item.editable">{{ item.is_alert }}</div>
               <v-switch
@@ -33,7 +42,7 @@
                 class="mt-0"
                 :value="item.is_alert === 'on'"
                 hide-details
-                @change="item.is_alert =  item.is_alert==='on'? 'off':'on'"
+                @change="item.is_alert = item.is_alert === 'on' ? 'off' : 'on'"
               ></v-switch>
             </template>
             <template #[`item.cpu_utilization`]="{ item }">
@@ -41,6 +50,20 @@
               <v-text-field
                 v-else
                 v-model="item.cpu_utilization"
+                type="number"
+                small
+                label=""
+                required
+                :rules="[
+                  (val) => (val || '').length > 0 || 'This field is required',
+                ]"
+              />
+            </template>
+            <template #[`item.memory_utilization`]="{ item }">
+              <div v-if="!item.editable">{{ item.memory_utilization }}</div>
+              <v-text-field
+                v-else
+                v-model="item.memory_utilization"
                 type="number"
                 small
                 label=""
@@ -78,18 +101,16 @@
                 ]"
               />
             </template>
-
-
             <template #[`item.operate`]="{ item }">
               <v-btn
                 small
                 :disabled="Object.keys(item).some((key) => item[key] === '')"
                 :color="item.editable ? 'secondary' : 'info'"
                 @click="
-                    Object.keys(item).every((key) => item[key])
-                      ? (item.editable = !item.editable)
-                      : (item.editable = true)
-                  "
+                  Object.keys(item).every((key) => item[key])
+                    ? (item.editable = !item.editable)
+                    : (item.editable = true)
+                "
               >
                 <v-icon size="20">mdi-pencil</v-icon>
                 <span class="d-none d-sm-inline-block">{{ $t('edit') }}</span>
@@ -117,31 +138,40 @@ export default {
     };
   },
   computed: {
-    headers(){
+    headers() {
       return [
         {
           text: this.$t('device.id'),
-          value: 'id'
+          value: 'id',
         },
         {
           text: this.$t('device.name'),
-          value: 'device' },
+          value: 'device',
+        },
         {
-
           text: this.$t('interface.alerting'),
-          value: 'is_alert' },
+          value: 'is_alert',
+        },
         {
           text: this.$t('cpu.usage'),
-          value: 'cpu_utilization' },
+          value: 'cpu_utilization',
+        },
+        {
+          text: this.$t('memory.usage'),
+          value: 'memory_utilization',
+        },
         {
           text: this.$t('high.temperature'),
-          value: 'temp_high' },
+          value: 'temp_high',
+        },
         {
           text: this.$t('low.temperature'),
-          value: 'temp_low' },
+          value: 'temp_low',
+        },
         {
           text: this.$t('operate'),
-          value: 'operate' },
+          value: 'operate',
+        },
       ];
     },
   },
