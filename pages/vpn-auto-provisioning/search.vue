4721bce7d3d3c88d63271b6d20134d0c8c732095
MSI\user <a0932384743@gmail.com>
2024-09-18 16:09:10 +0800

    VPN自動供裝設定

---
+++ pages/vpn-auto-provisioning/search.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container fluid>
+    <v-card class="px-10 py-5">
+      <v-card-title>
+        <h3>點對點 VPN 數量：3 筆</h3>
+      </v-card-title>
+      <v-data-table :headers="headers" :items="items" hide-default-footer>
+        <template #top>
+          <v-toolbar flat>
+            <v-toolbar-title>VPN List</v-toolbar-title>
+          </v-toolbar>
+        </template>
+      </v-data-table>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'VPNAutoProvisioningSearch',
+  layout: 'admin-layout',
+  data() {
+    return {
+      headers: [
+        { text: '#', value: 'index' },
+        { text: '申請單位', value: 'applicantUnit' },
+        { text: 'VLAN ID', value: 'vlanID' },
+        { text: '甲端', value: 'endpointA' },
+        { text: '乙端', value: 'endpointB' },
+        { text: '申請時間', value: 'applicationDate' },
+      ],
+      items: [
+        {
+          index: 1,
+          applicantUnit: '台大醫學中心',
+          vlanID: '101',
+          endpointA: '臺大GigaPOP',
+          endpointB: '成大GigaPOP',
+          applicationDate: '114/2/1',
+        },
+        {
+          index: 2,
+          applicantUnit: '中山大學',
+          vlanID: '102',
+          endpointA: '中山GigaPOP',
+          endpointB: '臺東GigaPOP',
+          applicationDate: '114/2/7',
+        },
+        {
+          index: 3,
+          applicantUnit: '台大計算機中心',
+          vlanID: '103',
+          endpointA: '臺大GigaPOP',
+          endpointB: '中山GigaPOP',
+          applicationDate: '114/5/1',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ pages/vpn-auto-provisioning/settings.vue
@@ -0,0 +1,142 @@
+<template>
+  <v-container fluid>
+    <v-form class="d-flex flex-column" style="gap: 2rem">
+      <v-card class="px-10 py-5">
+        <v-row>
+          <v-col cols="12">
+            <h3>點對點VPN連線：</h3>
+          </v-col>
+          <v-col cols="12" sm="4">
+            <v-select
+              v-model="applicantUnit"
+              color="info"
+              label="申請單位"
+              :items="['台大計算機中心']"
+            ></v-select>
+          </v-col>
+
+          <!-- VLAN ID -->
+          <v-col cols="12" sm="4">
+            <v-select
+              v-model="vlanID"
+              color="info"
+              label="VLAN ID"
+              :items="[
+                '100 (已指配)',
+                '101 (已指配)',
+                '102 (已指配)',
+                '103',
+                '104',
+                '105',
+              ]"
+            ></v-select>
+          </v-col>
+        </v-row>
+      </v-card>
+      <v-card class="px-10 py-5">
+        <!-- 甲端 -->
+        <v-row>
+          <v-col cols="12">
+            <h3>甲端：</h3>
+          </v-col>
+
+          <v-col cols="12" sm="4">
+            <v-select
+              v-model="gigaPOP_A"
+              color="info"
+              label="區網中心"
+              :items="['臺大GigaPOP']"
+            ></v-select>
+          </v-col>
+
+          <v-col cols="12" sm="4">
+            <v-select
+              v-model="pe1_A"
+              color="info"
+              label="PE1"
+              :items="['xe-1/0/0']"
+            ></v-select>
+          </v-col>
+
+          <v-col cols="12" sm="4">
+            <v-select
+              v-model="pe2_A"
+              color="info"
+              label="PE2"
+              :items="['xe-1/0/0']"
+            ></v-select>
+          </v-col>
+
+          <v-col cols="12" sm="4">
+            <v-text-field v-model="ce_A" label="CE"></v-text-field>
+          </v-col>
+        </v-row>
+      </v-card>
+      <v-card class="px-10 py-5">
+        <!-- 乙端 -->
+        <v-row>
+          <v-col cols="12">
+            <h3>乙端：</h3>
+          </v-col>
+
+          <v-col cols="12" sm="4">
+            <v-select
+              v-model="gigaPOP_B"
+              color="info"
+              label="區網中心"
+              :items="['中山GigaPOP']"
+            ></v-select>
+          </v-col>
+
+          <v-col cols="12" sm="4">
+            <v-select
+              v-model="pe1_B"
+              color="info"
+              label="PE1"
+              :items="['xe-1/3/0']"
+            ></v-select>
+          </v-col>
+
+          <v-col cols="12" sm="4">
+            <v-select
+              v-model="pe2_B"
+              color="info"
+              label="PE2"
+              :items="['xe-1/3/0']"
+            ></v-select>
+          </v-col>
+
+          <v-col cols="12" sm="4">
+            <v-select
+              v-model="ce_B"
+              color="info"
+              label="CE"
+              :items="['xe-1/3/0']"
+            ></v-select>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'VPNAutoProvisioningSettings',
+  layout: 'admin-layout',
+  data() {
+    return {
+      applicantUnit: '',
+      vlanID: '',
+      gigaPOP_A: '',
+      pe1_A: '',
+      pe2_A: '',
+      ce_A: '',
+      gigaPOP_B: '',
+      pe1_B: '',
+      pe2_B: '',
+      ce_B: '',
+    }
+  },
+}
+</script>
