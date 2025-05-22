5eaacb43e77715f441540deb08ced452e20d596c
a0932 <a0932384743@gmail.com>
2025-05-22 16:46:47 +0800

    產品型錄

--- pages/catalog.vue
+++ pages/catalog.vue
@@ -6,38 +6,45 @@
       :height="windowSize > 600 ? 360 : 195"
       position="left bottom"
     />
-    <v-container class="px-0">
+    <v-container class="px-0 mx-auto" style="max-width: 1360px">
       <v-breadcrumbs
         :items="menus"
         class="pt-0"
         :style="{
-          marginBottom: windowSize > 600 ? '150px' : '10px',
+          marginBottom: windowSize > 600 ? '235px' : '10px',
         }"
       >
         <template #divider>
           <v-icon color="#ba9545">mdi-chevron-right</v-icon>
         </template>
       </v-breadcrumbs>
-      <div class="title px-4 px-md-0">
-        <h1 class="d-block d-sm-none mx-auto mt-10">
+      <div class="title px-4 px-md-0 d-block d-md-none">
+        <h1 class="mb-2">
           {{ $t('word10') }}
         </h1>
-        <hr class="double-color-hr d-block d-sm-none" />
+        <hr class="double-color-hr"/>
       </div>
       <div
-        class="position-relative fill-width mx-auto py-16 catalog px-4 px-md-0"
+        class="position-relative mx-auto catalog fill-width"
         style="max-width: 1260px"
+        :style="{
+          marginBottom: windowSize > 960 ? '290px' : '0',
+        }"
       >
-        <v-img src="/image/catalog.png" width="100%" contain />
-        <div class="catalog-download text-start">
-          <h3 class="d-none d-sm-block">{{ $t('word10') }}</h3>
-          <v-divider class="d-none d-sm-block" style="border-color: black;" />
-          <v-btn text class="mr-auto d-none d-sm-block">
-            <arrow-icon direction="left" />
+        <v-img src="/image/catalog.png" width="100%" contain>
+          <div class="hover">
+            <div class="hover-img">DOWNLOAD</div>
+          </div>
+        </v-img>
+        <div class="catalog-download">
+          <h3 class="d-none d-md-block">{{ $t('word10') }}</h3>
+          <v-divider class="d-none d-md-block" style="border-color: black"/>
+          <v-btn text class="mr-auto d-none d-md-block">
+            <arrow-icon direction="left"/>
           </v-btn>
           <v-btn
             color="#D8AE5E"
-            class="d-block d-sm-none mx-auto mt-10"
+            class="d-block d-md-none mx-auto"
             max-width="135px"
             width="100%"
             dark
@@ -52,7 +59,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import {Component, Vue} from 'vue-property-decorator';
 import {
   EXTRA_SIZE,
   LARGE_SIZE,
@@ -63,14 +70,14 @@ import ArrowIcon from '../components/ArrowIcon.vue';
 
 @Component({
   name: 'catalog',
-  components: { ArrowIcon },
+  components: {ArrowIcon},
   layout: 'default-layout',
 })
 export default class catalog extends Vue {
   get menus() {
     return [
-      { text: this.$t('home'), disabled: false, href: '/' },
-      { text: this.$t('about.us'), disabled: true },
+      {text: this.$t('home'), disabled: false, href: '/'},
+      {text: this.$t('about.us'), disabled: true},
     ];
   }
 
@@ -115,27 +122,81 @@ export default class catalog extends Vue {
 .catalog {
   display: grid;
   grid-template-columns: minmax(500px, 1fr) minmax(140px, 1fr); /* 左 2 份，右 1 份 */
-  gap: 20px;
+  gap: 65px;
   align-items: end;
 
-  @media screen and (max-width: 780px) {
+  @media screen and (max-width: 960px) {
     & {
-      padding: 50px 40px;
       grid-template-columns: 1fr !important; /* 每列只有一欄 */
+      padding: 50px 40px 90px 40px;
+      gap: 50px;
     }
+  }
+}
+
+.catalog-download {
+  @media screen and (max-width: 960px) {
+    padding-top: 30px;
   }
 }
 
 h1 {
   font-weight: 500;
-  font-size: 18.39px;
+  font-size: 40px;
   line-height: 180%;
+  letter-spacing: 5px;
+
+  @media screen and (max-width: 600px) {
+    & {
+      font-size: 20px;
+    }
+  }
 }
 
 h3 {
   font-weight: 600;
   font-size: 45px;
   line-height: 180%;
+  letter-spacing: 5px;
   text-align: right;
+  color: #000000;
+}
+
+.hover:hover {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, .5) 100%, rgba(0, 0, 0, .5) 100%);
+  animation: hover 1s;
+}
+
+@keyframes hover {
+  0% {
+    background-size: 100% 0;
+  }
+  100% {
+    background-size:  100% 100%;
+  }
+}
+
+
+.hover-img {
+  background: rgba(0, 0, 0, 0.8) !important;
+  width: 260px;
+  height: 65px;
+  font-weight: 700;
+  font-size: 40px;
+  color: white;
+  margin: auto;
+  bottom: 1%;
+  left: calc(50% - 130px);
+  text-align: center;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media screen and (max-width: 600px) {
+    & {
+      font-size: 20px;
+    }
+  }
 }
 </style>
